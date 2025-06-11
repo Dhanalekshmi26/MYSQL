5b25fab280773137fa5d5679f744991b914594b8
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-11 22:17:32 +0530

    count in sql

---
+++ count.sql
@@ -0,0 +1,14 @@
+SELECT*FROM SCHOOLDETAILS.STUDENTS;       
++------------+------+------+
+| STUDENT_ID | NAME | AGE  |
++------------+------+------+
+|          1 | RAVI |   20 |
+|          2 | NEHA |   21 |
++------------+------+------+
+
+SELECT COUNT(*) FROM SCHOOLDETAILS.students;  
++----------+
+| COUNT(*) |
++----------+
+|        2 |
++----------+
