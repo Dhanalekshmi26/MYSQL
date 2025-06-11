2444e06d084edbf2a9750902c03d5f2f6340f080
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-11 22:19:35 +0530

    sum function used to add

---
+++ sum.sql
@@ -0,0 +1,19 @@
+SELECT*FROM SCHOOLDETAILS.MARKS;          
++---------+-----------+-------+------------+
+| MARK_ID | SUBJECT   | SCORE | STUDENT_ID |
++---------+-----------+-------+------------+
+|       1 | PHYSICS   |    95 |          1 |
+|       1 | CHEMISTRY |    98 |          1 |
+|       3 | MATHS     |   100 |          1 |
+|       4 | MATHS     |    99 |          2 |
+|       5 | CHEMISTRY |   100 |          2 |
+|       6 | PHYSICS   |    99 |          2 |
++---------+-----------+-------+------------+
+
+
+SELECT SUM(score) FROM SCHOOLDETAILS.marks; 
++------------+
+| SUM(score) |
++------------+
+|        591 |
++------------+
