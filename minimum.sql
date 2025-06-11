9b6506d7c25c9e5ecf1238955dd2a46372dee2ac
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-11 22:22:30 +0530

    to find minimum

---
+++ minimum.sql
@@ -0,0 +1,18 @@
+SELECT*FROM SCHOOLDETAILS.MARKS;          
++---------+-----------+-------+------------+
+| MARK_ID | SUBJECT   | SCORE | STUDENT_ID |
++---------+-----------+-------+------------+
+|       1 | PHYSICS   |    95 |          1 |
+|       1 | CHEMISTRY |    98 |          1 |
+|       3 | MATHS     |   100 |          1 |
+|       4 | MATHS     |    99 |          2 |
+|       5 | CHEMISTRY |   100 |          2 |
+|       6 | PHYSICS   |    99 |          2 |
++---------+-----------+-------+------------+
+
+SELECT MIN(score) FROM SCHOOLDETAILS.marks; 
++------------+
+| MIN(score) |
++------------+
+|         95 |
++------------+
